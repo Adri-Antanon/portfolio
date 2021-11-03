1b2edda6f09cfd73ba263308be9e33e9412d4f99
Adrian Antanon Orozco <aantanon@brothapps.com>
2021-11-03 11:36:29 +0100

    feat: :star: add one more nextJs project

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,55 +1,53 @@
-import React from 'react';
+import React from "react";
 
-import ImageSection from '../components/ImageSection';
-import SkillsSection from '../components/SkillsSection';
-import SoftSkillsSection from '../components/SoftSkillsSection';
-import Title from '../components/Title';
+import ImageSection from "../components/ImageSection";
+import SkillsSection from "../components/SkillsSection";
+import SoftSkillsSection from "../components/SoftSkillsSection";
+import Title from "../components/Title";
 
-import teamwork from '../assets/img/teamwork.svg';
-import adaptability from '../assets/img/adaptability.svg';
-import communication from '../assets/img/communication.svg';
+import teamwork from "../assets/img/teamwork.svg";
+import adaptability from "../assets/img/adaptability.svg";
+import communication from "../assets/img/communication.svg";
 
 const AboutPage = () => {
-
   return (
     <div className="AboutPage">
-      <Title
-        title={'About Me'}
-        span={'About Me'}
-      />
+      <Title title={"About Me"} span={"About Me"} />
       <ImageSection />
-      <Title title={'Skills'} span={'Skills'} />
+      <Title title={"Skills"} span={"Skills"} />
       <div className="SkillsContainer">
-        <SkillsSection skill={'JavaScript'} progress={'70%'} width={'70%'} />
-        <SkillsSection skill={'React Js'} progress={'65%'} width={'65%'} />
-        <SkillsSection skill={'React Native'} progress={'55%'} width={'55%'} />
-        <SkillsSection skill={'TypeScript'} progress={'50%'} width={'50%'} />
-        <SkillsSection skill={'Angular 2+'} progress={'40%'} width={'40%'} />
-        <SkillsSection skill={'HTML5'} progress={'80%'} width={'80%'} />
-        <SkillsSection skill={'CSS3'} progress={'70%'} width={'70%'} />
-        <SkillsSection skill={'SASS'} progress={'50%'} width={'50%'} />
-        <SkillsSection skill={'Java'} progress={'60%'} width={'60%'} />
-        <SkillsSection skill={'PHP'} progress={'30%'} width={'30%'} />
-        <SkillsSection skill={'SQL'} progress={'60%'} width={'60%'} />
-        <SkillsSection skill={'NoSQL'} progress={'40%'} width={'40%'} />
-        <SkillsSection skill={'Git'} progress={'90%'} width={'90%'} />
+        {/* Cambiar porcentajes por nombre - icono, mucho mejor que mostrar un porcentaje subjetivo de mis conocimientos */}
+        <SkillsSection skill={"JavaScript"} progress={"80%"} width={"80%"} />
+        <SkillsSection skill={"React Js"} progress={"75%"} width={"75%"} />
+        <SkillsSection skill={"Next Js"} progress={"50%"} width={"50%"} />
+        <SkillsSection skill={"React Native"} progress={"55%"} width={"55%"} />
+        <SkillsSection skill={"TypeScript"} progress={"50%"} width={"50%"} />
+        <SkillsSection skill={"Angular 2+"} progress={"40%"} width={"40%"} />
+        <SkillsSection skill={"HTML5"} progress={"80%"} width={"80%"} />
+        <SkillsSection skill={"CSS3"} progress={"70%"} width={"70%"} />
+        <SkillsSection skill={"SASS"} progress={"50%"} width={"50%"} />
+        <SkillsSection skill={"Java"} progress={"60%"} width={"60%"} />
+        <SkillsSection skill={"PHP"} progress={"30%"} width={"30%"} />
+        <SkillsSection skill={"SQL"} progress={"60%"} width={"60%"} />
+        <SkillsSection skill={"NoSQL"} progress={"40%"} width={"40%"} />
+        <SkillsSection skill={"Git"} progress={"90%"} width={"90%"} />
       </div>
-      <Title title={'Soft Skills'} span={'Soft'} />
+      <Title title={"Soft Skills"} span={"Soft"} />
       <div className="SoftSkillsContainer">
         <SoftSkillsSection
           image={teamwork}
-          title={'Teamwork'}
-          text={'Nurture healthy work environments and help each other.'}
+          title={"Teamwork"}
+          text={"Nurture healthy work environments and help each other."}
         />
         <SoftSkillsSection
           image={adaptability}
-          title={'Adaptability'}
+          title={"Adaptability"}
           text={"Try new tools and techniques to improve everyone's work"}
         />
         <SoftSkillsSection
           image={communication}
-          title={'Communication'}
-          text={'A concise email helps avoid back and forth messages.'}
+          title={"Communication"}
+          text={"A concise email helps avoid back and forth messages."}
         />
       </div>
     </div>
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,45 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import webDevProjects from '../data/allProjects';
-import Categories from '../components/Categories';
-import MenuItems from '../components/MenuItems';
-import Title from '../components/Title';
+import webDevProjects from "../data/allProjects";
+import Categories from "../components/Categories";
+import MenuItems from "../components/MenuItems";
+import Title from "../components/Title";
 
-const allCategories = ['All', ...new Set(
-  webDevProjects.map(item => item.category)
-)];
+const allCategories = [
+  "All",
+  ...new Set(webDevProjects.map((item) => item.category)),
+];
 
 const ProjectsPage = () => {
-  const [categories, setCategories] = useState(allCategories);
   const [menuItems, setMenuItems] = useState(webDevProjects);
 
   const filter = (category) => {
-    if (category === 'All') {
-      setMenuItems(webDevProjects)
+    if (category === "All") {
+      setMenuItems(webDevProjects);
       return;
     }
     const filteredData = webDevProjects.filter((item) => {
       return item.category === category;
-    })
+    });
     setMenuItems(filteredData);
-  }
+  };
 
   return (
-    <div className="ProjectsPage" >
+    <div className="ProjectsPage">
       <div className="title">
-        <Title
-          title={'Projects'}
-          span={'projects'}
-        />
+        <Title title={"Projects"} span={"projects"} />
       </div>
       <div className="projects-data">
-        <Categories
-          filter={filter}
-          categories={categories}
-        />
-        <MenuItems
-          menuItem={menuItems}
-        />
+        <Categories filter={filter} categories={allCategories} />
+        <MenuItems menuItem={menuItems} />
       </div>
     </div>
   );
