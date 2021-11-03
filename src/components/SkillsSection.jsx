83582dba4fe64b20169bff448d341599a9062753
Adrian Antanon Orozco <aantanon@brothapps.com>
2021-11-03 12:51:01 +0100

    refactor: :hammer: rebuild hard skills section & add skills logos

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React from "react";
 
-const SkillsSection = (props) => {
-  const { skill, progress, width } = props;
+import { HARD_SKILLS } from "../data/allSkills";
+
+const SkillsSection = () => {
+  // const { skill, progress, width } = props;
+
+  // Hark skills en formato de barras
+  // const chartContent = (
+  //   <div className="SkillsSection">
+  //     <div className="skills-container">
+  //       <h5 className="skill-title">{skill}</h5>
+  //       <div className="skill-bar">
+  //         <p className="skill-text">{progress}</p>
+  //         <div className="skill-progress">
+  //           <div className="progress">
+  //             <div className="inner-pregress" style={{ width: width }}></div>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
+
   return (
-    <div className="SkillsSection">
-      <div className="skills-container">
-        <h5 className="skill-title">{skill}</h5>
-        <div className="skill-bar">
-          <p className="skill-text">{progress}</p>
-          <div className="skill-progress">
-            <div className="progress">
-              <div className="inner-pregress" style={{ width: width }}></div>
-            </div>
-          </div>
+    <div className="skills-container">
+      {HARD_SKILLS.map((skill) => (
+        <div className="skills-item">
+          <img src={skill.src} alt={skill.name} />
+          <p>{skill.name}</p>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -15,23 +15,7 @@ const AboutPage = () => {
       <Title title={"About Me"} span={"About Me"} />
       <ImageSection />
       <Title title={"Skills"} span={"Skills"} />
-      <div className="SkillsContainer">
-        {/* Cambiar porcentajes por nombre - icono, mucho mejor que mostrar un porcentaje subjetivo de mis conocimientos */}
-        <SkillsSection skill={"JavaScript"} progress={"80%"} width={"80%"} />
-        <SkillsSection skill={"React Js"} progress={"75%"} width={"75%"} />
-        <SkillsSection skill={"Next Js"} progress={"50%"} width={"50%"} />
-        <SkillsSection skill={"React Native"} progress={"55%"} width={"55%"} />
-        <SkillsSection skill={"TypeScript"} progress={"50%"} width={"50%"} />
-        <SkillsSection skill={"Angular 2+"} progress={"40%"} width={"40%"} />
-        <SkillsSection skill={"HTML5"} progress={"80%"} width={"80%"} />
-        <SkillsSection skill={"CSS3"} progress={"70%"} width={"70%"} />
-        <SkillsSection skill={"SASS"} progress={"50%"} width={"50%"} />
-        <SkillsSection skill={"Java"} progress={"60%"} width={"60%"} />
-        <SkillsSection skill={"PHP"} progress={"30%"} width={"30%"} />
-        <SkillsSection skill={"SQL"} progress={"60%"} width={"60%"} />
-        <SkillsSection skill={"NoSQL"} progress={"40%"} width={"40%"} />
-        <SkillsSection skill={"Git"} progress={"90%"} width={"90%"} />
-      </div>
+      <SkillsSection />
       <Title title={"Soft Skills"} span={"Soft"} />
       <div className="SoftSkillsContainer">
         <SoftSkillsSection
